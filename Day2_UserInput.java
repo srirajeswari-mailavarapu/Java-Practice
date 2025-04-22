c0370536b5985eef5696867a841ab07f7cafd495
Mailavarapu Sri Rajeswari <mailavarapusrirajeswari@gmail.com>
2025-04-22 17:17:40 +0530

    Create Day2_UserInput.java
    This program demonstrates how to take input from the user using the 'Scanner' class in java.
    It reads the user's name and prints a welcome message

---
+++ Day2_UserInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Day2_UserInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name + "! Welcome to Java.");
+
+        sc.close();
+    }
+}
